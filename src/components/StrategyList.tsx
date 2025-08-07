46ab24cdc6cd3c6475668c02b6786ef06bc5e516
akghanekar000 <akghanekar000@gmail.com>
2025-08-07 17:24:16 +0530

    chore: scaffold project structure with boilerplate files

---
+++ src/components/StrategyList.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const StrategyList: React.FC = () => {
+  return <div>Strategy List Component</div>;
+};
+
+export default StrategyList;
+
+// Placeholder for listing strategies.
