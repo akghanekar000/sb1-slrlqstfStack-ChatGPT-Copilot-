30d56663e694b25df4bd1a26fe3242d09ad959aa
akghanekar000 <akghanekar000@gmail.com>
2025-08-07 17:24:22 +0530

    chore: scaffold project structure with boilerplate files

---
+++ src/components/StrategyForm.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const StrategyForm: React.FC = () => {
+  return <form>Strategy Form Component</form>;
+};
+
+export default StrategyForm;
+
+// Placeholder for strategy form.
